a925d6722753b8e04f1da05e30101a63fea2b0ab
Carlos Alvarez Gonzalez <LitosLpr@gmail.com>
2018-02-16 13:12:10 +0100

    Adaptado a mi calendario DisplayDosCaracteres

--- CalendarioBasico.java
+++ CalendarioBasico.java
@@ -10,20 +10,19 @@
 public class CalendarioBasico
 {
     // Save the dia.
-    private int dia;
+    private DisplayDosCaracteres dia;
     // Save the mes.
-    private int mes;
+    private DisplayDosCaracteres mes;
     // Save the ano.
-    private int ano;
+    private DisplayDosCaracteres ano;
     /**
      * Constructor for objects of class CalendarioBasico.
      **/
     public CalendarioBasico()
     {
-        dia = 1;
-        mes = 1;
-        //comes in 2100 
-        ano = 1; //primer año
+        dia = new DisplayDosCaracteres (31);
+        mes = new DisplayDosCaracteres (13);
+        ano = new DisplayDosCaracteres (100);
     }
 
     /**
@@ -31,55 +30,33 @@ public class CalendarioBasico
      */
     public String obtenerFecha ()
     {
-        // Create a text chain like: '1dd-1mm-1yy'.
-        String date = (dia+100) + "-" + (mes+100) + "-" + (ano+100);
-        // Now, ignore '1' and take the rest.
-        date = date.substring(1,4) 
-        + date.substring(5,8) 
-        + date.substring(9,11);
-        return date;
+        return dia.getTextoDelDisplay() + "-" 
+        + mes.getTextoDelDisplay() + "-" 
+        + ano.getTextoDelDisplay();
     }
 
     /**
      ** Introduccion de datos por parte del usuario.
-     */
-    public void fijarFecha (int setDia, int setMes, int setAno)
+     **/
+    public void fijarFecha (int setDay, int setMonth, int setYear)
     {
-        dia = setDia;
-        mes = setMes;
-        //value between 2000 - 2099
-        ano = setAno;
+        dia.setValorAlmacenado(setDay);
+        mes.setValorAlmacenado(setMonth);
+        ano.setValorAlmacenado(setYear);
     }
 
-    /**
-     * * Set a specific date.
-     */
     public void avanzarFecha ()
     {
-        // Check if adding a dia changes the mes or ano.
-        if (dia == 30)
         {
-            dia = 01;
-            if (mes == 12) 
-            {
-                mes = 01;
-                ano += 1;                
+            // Check if adding a dia changes the mes or ano. 
+            if (dia.getValorAlmacenado() == 30) {
+                mes.incrementaValorAlmacenado();
+                if (mes.getValorAlmacenado() == 1){
+                    ano.incrementaValorAlmacenado();}
             }
-            else 
-            {
-                mes += 1;
-            }
+
         }
-        // 'Cristian hates me' validation. 
-        // Stop the eschaton, 2100 never comes!
-        else if (dia == 30 && mes == 12 && ano == 99)
-        {
-            dia = 1;
-            mes = 1;
-            ano = 0;
-        }
-        else {
-            dia += 1;
-        }
+        dia.incrementaValorAlmacenado();
     }
 }
+    
